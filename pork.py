af7a9b0aaca1613a12aa40f9ef13f2bd52a16f29
Austin <17520250+bombercode@users.noreply.github.com>
2024-12-28 19:08:20 -0500

    Continued work on logic
    Sections 0-2 logic done, game map completed internally. Code linted and many changes made to help with consistency per linting.

--- pork.py
+++ pork.py
@@ -1,39 +1,44 @@
 '''
-Pork, a Zork like game about a pig
+Pork, a Zork like game about a pig named Hammy
 Initial code by bombercode
 '''
-# Importing the art lib to make the game title look a little cooler
-from art import *
-from colored import fg, attr #Importing colored to help color some text
-import porklogic #Importing functions from game logic file
-tprint("PORK", space=3)
-health = 3 #Making this a global variable so it can be touched by all functions if needed
-#Creating master function to run game
+# Library imports
+from art import *  # Importing the art lib to make the game title look a little cooler
+from colored import fg, attr  # Importing colored to help color some text
+import porklogic  # Importing functions from game logic file
+tprint("PORK", space=3)  # Printing the game title, using tprint to make it look fancy
+health = 3  # Making this a global variable so it can be touched by all functions if needed
+# Creating master function to run game
 def runGame():
-#Intro text
+# Intro text
     print("You are named Hammy\n")
     print("You are a pig and you must make your way through the forest.\n")
+    print("You do not have time to backtrack so choose wisely!\n")
     print("Don't become bacon!\n")
-#Creating a while loop to keep track of health and end the game if Hammy dies
+# Creating a while loop to keep track of health and end the game if Hammy dies
     while health != 0:
         porklogic.sectionZero()
 # Tracking player health game over condition here
         if health <= 0:
             print("You have become bacon!")
             break
-#Getting yes or no input for game start
-#gameTitle function contains the game start secion so invalid inputs do not result in an exit
+# Getting yes or no input for game start
+# gameTitle function contains the game start secion so invalid inputs do not result in an exit
+
+
 def gameTitle():
-    gameStart = input("Do you dare enter? (y/n)")
-#Actions resulting from input
+    gameStart = input("Do you dare enter? (y/n)\n")
+# Actions resulting from input
     if gameStart == "y":
         print("Welcome!")
-        runGame() #This launches the main part of the game defined above as runGame for the actual game to start
+        runGame()  # This launches the main part of the game defined above
     elif gameStart == "n":
         print("Farewell!")
         exit()
     else:
         print("Valid inputs are y or n")
-        gameTitle() #This allows invalid key inputs to loop back for another input
-#Running gameTitle funciton to start the game
+        gameTitle()  # This allows invalid key inputs to loop back for another input
+
+
+# Running gameTitle funciton to start the game
 gameTitle()
